c267be556d42c88fad71051118b4140b231f7166
Svetlana <kozhevnikova-svetlana85@bk.ru>
2022-08-14 20:26:22 +0300

    added task4

---
+++ task4/Program.cs
@@ -0,0 +1,20 @@
+﻿// задача 15, второй способ
+Console.WriteLine("Введите число, обозначающий день недели: ");
+int dayOfWeek = Convert.ToInt32(Console.ReadLine());
+
+void CheckingDayOfWeek (int dayOfWeek)
+{
+  if (dayOfWeek > 0 && dayOfWeek <= 5)
+  {
+    Console.WriteLine("Этот день не выходной, нет");
+  }
+   else if (dayOfWeek == 6  || dayOfWeek == 7)
+ {
+    Console.WriteLine("Этот день выходной, Да");
+ }
+ else
+{
+    Console.WriteLine("Такого дня нет");
+}
+}
+ CheckingDayOfWeek (dayOfWeek);
